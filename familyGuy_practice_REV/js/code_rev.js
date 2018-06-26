70dd0171a005b72e82be9a80958668ca04c225b3
Aaron <agoldman12@gmail.com>
2018-06-25 20:39:43 -0500

    Update code_rev.js
    Now I am using classList.toggle() and a switch statement to control which "charInfo" div is shown. I am using the id of the <h2> 'button' as my case and if it matches the id of the 'charInfo' div, then that div is toggled.

--- familyGuy_practice_REV/js/code_rev.js
+++ familyGuy_practice_REV/js/code_rev.js
@@ -1,43 +1,4 @@
 'use strict';
-/***********************************************************
-      Code below is where I am now
-      - Three different functions, one for each character
-************************************************************
-//function to show / hide peter griffin 
-function togglePeter(){
-    const peter = document.getElementById('peter');
-    peter.classList.toggle('isHidden');
-    
-}
-const pg_btn = document.getElementById('pg_btn');
-pg_btn.addEventListener('click', togglePeter);
-
-//function to show / hide Chris griffin 
-function toggleChris(){
-    
-    const chris = document.getElementById('chris');
-    chris.classList.toggle('isHidden');
-    
-}
-const cg_btn = document.getElementById('cg_btn');
-cg_btn.addEventListener('click', toggleChris);
-
-//function to show / hide Brian griffin 
-function toggleBrian(){
-    
-    const brian = document.getElementById('brian');
-        brian.classList.toggle('isHidden');
-}
-const bg_btn = document.getElementById('bg_btn');
-bg_btn.addEventListener('click', toggleBrian);
-
-/************************************************************
-      Code below is where I want to go
-      - single function to show/hide all character info
-*************************************************************/
-
-//This code loops through all charinfo divs and toggles isHidden. However it does it to all three at the same time.
-//Need to figure out how to use nextSibling (or something similar).
 
 //reference for <h2> that I want to use as a button
 const button = document.getElementsByClassName("charHead");
@@ -45,9 +6,30 @@ for (var i = 0; i < button.length; i++) {
       button[i].addEventListener('click', toggleDisplay, false);
    }
 
-function toggleDisplay(){
-      let charInfo = document.getElementsByClassName('charInfo');
-      for(var i = 0; i < charInfo.length; i++){
-        charInfo[i].classList.toggle('isHidden');
-      }
-}
+//show and hide image / paragraph for each character
+function toggleDisplay() {
+    let id = this.id;
+    switch (id) {
+        case "pg_btn": { //id of button
+            const peter = document.getElementById("peter"); //id of charInfo div
+            peter.classList.toggle("isHidden");
+        };
+        break;
+        case "cg_btn": { //id of button
+            const chris = document.getElementById("chris"); //id of charInfo div
+            chris.classList.toggle("isHidden");
+        };
+        break;
+        case "bg_btn": { //id of button
+            const brian = document.getElementById("brian"); //id of charInfo div
+            brian.classList.toggle("isHidden");
+        };
+        break;
+    }
+  }
+
+
+/*
+Source for toggle with classList and Switch
+http://dh.obdurodon.org/javascript/classListToggle.xhtml
+*/
