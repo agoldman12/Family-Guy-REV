1688d238a603608e453453b5817a8f0c528ce3ef
Aaron <agoldman12@gmail.com>
2018-06-26 21:46:37 -0500

    Replace Switch with if/else
    The toggleDisplay function stores the id of the button that was clicked into a variable. Then it loops through the charInfo array and checks to see if the button id and current charInfo div id both equal one character (peter, chris, or brian). If so, it gets a reference to the charInfo div's id and toggles the class 'isHidden' for that div.

--- familyGuy_practice_REV/js/code_rev.js
+++ familyGuy_practice_REV/js/code_rev.js
@@ -6,30 +6,29 @@ for (var i = 0; i < button.length; i++) {
       button[i].addEventListener('click', toggleDisplay, false);
    }
 
-//show and hide image / paragraph for each character
-function toggleDisplay() {
-    let id = this.id;
-    switch (id) {
-        case "pg_btn": { //id of button
-            const peter = document.getElementById("peter"); //id of charInfo div
-            peter.classList.toggle("isHidden");
-        };
-        break;
-        case "cg_btn": { //id of button
-            const chris = document.getElementById("chris"); //id of charInfo div
-            chris.classList.toggle("isHidden");
-        };
-        break;
-        case "bg_btn": { //id of button
-            const brian = document.getElementById("brian"); //id of charInfo div
-            brian.classList.toggle("isHidden");
-        };
-        break;
+//function to show / hide image and paragraph for each character
+function toggleDisplay(){
+    const charInfo = document.getElementsByClassName('charInfo');
+    const btn_id = this.id;
+    
+    for(var i = 0; i < charInfo.length; i++){
+        if(btn_id === "pg_btn" && charInfo[i].id === "peter"){
+            const peter = document.getElementById("peter");
+            peter.classList.toggle('isHidden');
+        }else if(btn_id === "cg_btn" && charInfo[i].id === "chris"){
+            const chris = document.getElementById("chris");
+            chris.classList.toggle('isHidden');
+        }else{
+            const brian = document.getElementById("brian");
+            brian.classList.toggle('isHidden');
+        }
     }
-  }
+}
+
 
 
 /*
-Source for toggle with classList and Switch
+code above based on the following:
+Source for toggle with classList and Switch 
 http://dh.obdurodon.org/javascript/classListToggle.xhtml
 */
